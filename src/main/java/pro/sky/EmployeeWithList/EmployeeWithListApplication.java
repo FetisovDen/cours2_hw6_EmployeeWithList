809d557d30f582275119c2d6fa6a61f21d8a0722
555 <fetisovden@yandex.ru>
2022-09-23 15:48:14 +0300

    первый коммит

---
+++ src/main/java/pro/sky/EmployeeWithList/EmployeeWithListApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.EmployeeWithList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWithListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWithListApplication.class, args);
+	}
+
+}
